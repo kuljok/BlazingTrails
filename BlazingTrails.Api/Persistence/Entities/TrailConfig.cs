304bbf844c3b8f5f5d2fa472ccfb32c66e924e66
Andrei Kuleuski <kuljok@gmail.com>
2023-04-22 17:11:09 +0300

    Configuring the initial entities for the system

---
+++ BlazingTrails.Api/Persistence/Entities/TrailConfig.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlazingTrails.Api.Persistence.Entities;
+
+public class TrailConfig: IEntityTypeConfiguration<Trail>
+{
+    public void Configure(EntityTypeBuilder<Trail> builder)
+    {
+        builder.Property(x => x.Name).IsRequired();
+        builder.Property(x => x.Description).IsRequired();
+        builder.Property(x => x.Location).IsRequired();
+        builder.Property(x => x.TimeInMinutes).IsRequired();
+        builder.Property(x => x.Length).IsRequired();
+    }
+}
