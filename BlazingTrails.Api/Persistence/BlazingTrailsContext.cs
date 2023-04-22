991241016907b94c37866b61ced0d5b1371dd286
Andrei Kuleuski <kuljok@gmail.com>
2023-04-22 17:17:28 +0300

    Setting up the database context

--- BlazingTrails.Api/Persistence/BlazingTrailsContext.cs
+++ BlazingTrails.Api/Persistence/BlazingTrailsContext.cs
@@ -1,6 +1,23 @@
+using BlazingTrails.Api.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+using RouteInstruction = BlazingTrails.Client.Features.Home.RouteInstruction;
+
 namespace BlazingTrails.Api.Persistence;
 
-public class BlazingTrailsContext
+public class BlazingTrailsContext: DbContext
 {
-    
+    public DbSet<Trail> Trails => Set<Trail>();
+    public DbSet<RouteInstruction> RouteInstructions => Set<RouteInstruction>();
+
+    public BlazingTrailsContext(DbContextOptions<BlazingTrailsContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new TrailConfig());
+        modelBuilder.ApplyConfiguration(new RouteInstructionConfig());
+    }
 }
