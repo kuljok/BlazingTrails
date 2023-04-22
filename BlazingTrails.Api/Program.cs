0a67ab6a8c6b5b5c4f5e8be9cd535d98ca548456
Andrei Kuleuski <kuljok@gmail.com>
2023-04-22 16:43:24 +0300

    Adding an ASP.NET Core backend to a Blazor WebAssembly app

---
+++ BlazingTrails.Api/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // Enable the debugging of Blazor WebAssembly code
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+// enables the API to serve the Blazor application
+app.UseBlazorFrameworkFiles();
+// enables static files to be served by the API
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+// if a request doesn't match a controller, 
+// serve the index.html file from the Blazor project
+app.MapFallbackToFile("index.html");
+
+app.Run();
